ae1a188da426d0f2e65e73b2261b609d8917640b
pinguxx28 <riskumax@gmail.com>
2022-03-23 21:18:14 +0200

    now 18 colors

--- hdr/color.hpp
+++ hdr/color.hpp
@@ -4,32 +4,50 @@
 
 namespace color
 {
+    typedef unsigned char u_char;
+
     ALLEGRO_COLOR rgb(u_char r, u_char g, u_char b);
     ALLEGRO_COLOR rgbf(float r, float g, float b);
     ALLEGRO_COLOR rgb_f(float r, float g, float b);
 
-    enum colorIndexes
+    typedef enum colorIndex
     {
-        whiteI  = 1,
-        blackI,
-        greyI,
+        darkredI = 1,
         redI,
         orangeI,
         yellowI,
         greenI,
+        darkgreenI,
         blueI,
+        darkblueI,
         purpleI,
-    };
+        pinkI,
+        lightgrayI,
+        grayI,
+        darkgrayI,
+        whiteI,
+        beigeI,
+        brownI,
+        blackI,
+    } colorIndex;
     
-    const ALLEGRO_COLOR red    = rgbf(1, 0, 0);
-    const ALLEGRO_COLOR orange = rgbf(1, 0.5, 0);
-    const ALLEGRO_COLOR yellow = rgbf(1, 1, 0);
-    const ALLEGRO_COLOR green  = rgbf(0, 1, 0);
-    const ALLEGRO_COLOR blue   = rgbf(0, 0, 1);
-    const ALLEGRO_COLOR purple = rgbf(1, 0, 1);
-    const ALLEGRO_COLOR white  = rgbf(1, 1, 1);
-    const ALLEGRO_COLOR black  = rgbf(0, 0, 0);
-    const ALLEGRO_COLOR grey   = rgbf(0.5, 0.5, 0.5);
+    const ALLEGRO_COLOR darkred   = rgb(128,0,0);
+    const ALLEGRO_COLOR red       = rgb(255, 0, 0);
+    const ALLEGRO_COLOR orange    = rgb(255, 140, 0);
+    const ALLEGRO_COLOR yellow    = rgb(255, 255, 0);
+    const ALLEGRO_COLOR green     = rgb(0, 255, 0);
+    const ALLEGRO_COLOR darkgreen = rgb(0, 128, 0);
+    const ALLEGRO_COLOR blue      = rgb(0, 0, 255);
+    const ALLEGRO_COLOR darkblue  = rgb(0, 0, 128);
+    const ALLEGRO_COLOR purple    = rgb(147, 112, 219);
+    const ALLEGRO_COLOR pink      = rgb(255, 20, 147);
+    const ALLEGRO_COLOR lightgray = rgb(211, 211, 211);
+    const ALLEGRO_COLOR gray      = rgb(127, 127, 127);
+    const ALLEGRO_COLOR darkgray  = rgb(105, 105, 105);
+    const ALLEGRO_COLOR white     = rgb(250, 250, 250);
+    const ALLEGRO_COLOR beige     = rgb(210, 180, 140);
+    const ALLEGRO_COLOR brown     = rgb(160, 82, 45);
+    const ALLEGRO_COLOR black     = rgb(0, 0 ,0);
 
     ALLEGRO_COLOR index(u_char index);
 }
--- src/hdr/color.cpp
+++ src/hdr/color.cpp
@@ -9,15 +9,23 @@ ALLEGRO_COLOR color::index(u_char index)
     ALLEGRO_COLOR color = color::rgbf(0, 0, 0);
     switch (index)
     {
-        case color::redI   : color = color::red   ; break;
-        case color::orangeI: color = color::orange; break;
-        case color::yellowI: color = color::yellow; break;
-        case color::greenI : color = color::green ; break;
-        case color::blueI  : color = color::blue  ; break;
-        case color::purpleI: color = color::purple; break;
-        case color::whiteI : color = color::white ; break;
-        case color::blackI : color = color::black ; break;
-        case color::greyI  : color = color::grey  ; break;
+        case color::darkredI  : color = color::darkred  ; break;
+        case color::redI      : color = color::red      ; break;
+        case color::orangeI   : color = color::orange   ; break;
+        case color::yellowI   : color = color::yellow   ; break;
+        case color::greenI    : color = color::green    ; break;
+        case color::darkgreenI: color = color::darkgreen; break;
+        case color::blueI     : color = color::blue     ; break;
+        case color::darkblueI : color = color::darkblue ; break;
+        case color::purpleI   : color = color::purple   ; break;
+        case color::pinkI     : color = color::pink     ; break;
+        case color::lightgrayI: color = color::lightgray; break;
+        case color::grayI     : color = color::gray     ; break;
+        case color::darkgrayI : color = color::darkgray ; break;
+        case color::whiteI    : color = color::white    ; break;
+        case color::beigeI    : color = color::beige    ; break;
+        case color::brownI    : color = color::brown    ; break;
+        case color::blackI    : color = color::black    ; break;
     }
     return color;
 }
